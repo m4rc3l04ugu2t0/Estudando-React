3d40b10e26ee6d53aa703b7da7d060222d0c1f61
mrcelo014@gmail.com <mrcelo014@gmail.com>
2023-09-30 14:12:19 -0300

    creating the second hook

---
+++ Studying-React/creatingHook 2/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { App } from '/Studying-React/creatingHook 2/App.jsx'
+
+ReactDOM.render (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('react-app')
+)
--- Studying-React/organization/components/CounterProvider/reducer/index.jsx
+++ Studying-React/organization/components/CounterProvider/reducer/index.jsx
@@ -1,6 +1,7 @@
 import { actionsTypes } from '/Studying-React/organization/components/CounterProvider/actionTypes/index.jsx'
 
 export const reducer = (state, action) => {
+  
   switch (action.type) {
     case actionsTypes.INCREMENT_COUNTER:
       return { ...state, counter: state.counter + 1 }
